e465e1d930e48fef5313eb11e28c0aad4adb3c36
autoseclabs <autoseclabs@gmail.com>
2025-09-06 12:29:07 +0530

    country flags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; 2024 AutoSecLabs. All rights reserved.</p>
+          <p>&copy; 2025 AutoSecLabs. All rights reserved.</p>
         </div>
       </div>
     </footer>
